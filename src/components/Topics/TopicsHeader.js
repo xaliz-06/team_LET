cfbdf8c677f1a618d52fcd504229f7208c1ca69d
xaliz-06 <vaibhavchakraborty06@gmail.com>
2023-12-15 23:39:48 +0530

    adding landing files

--- src/components/Topics/TopicsHeader.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const TopicsHeader = () => {
-  return <div>TopicsHeader</div>;
-};
-
-export default TopicsHeader;
--- src/ui/Break.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import styles from "./Break.module.css";
-
-const Break = () => {
-  return <div className={styles.break_style}></div>;
-};
-
-export default Break;
